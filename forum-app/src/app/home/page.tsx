0b46443f219006d09d0219d582a2f40ba84a9f06
kijima-riku <riku.bulls.0116@icloud.com>
2024-09-20 05:34:21 +0900

    [add] sample data.

--- forum-app/src/app/home/page.tsx
+++ forum-app/src/app/home/page.tsx
@@ -119,6 +119,54 @@ export default function Home(): JSX.Element {
         "https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Donald_Trump_official_portrait.jpg/250px-Donald_Trump_official_portrait.jpg",
       description: "",
     },
+    {
+      subChubuForum: "r/AskChubuForum",
+      timeAgo: "8 hr. ago",
+      title:
+        "You're the last men/women on earth. Everyone else has the opposite sex. What do you do?",
+      upvotes: "3.6K",
+      comments: "5.2K",
+      imageUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Donald_Trump_official_portrait.jpg/250px-Donald_Trump_official_portrait.jpg",
+      description: "",
+    },
+    {
+      subChubuForum: "r/baseball",
+      timeAgo: "2 hr. ago",
+      title:
+        "[Highlight] Back to back to back home run from Shohei Ohtani, Mookie Betts & Freddie Freeman",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Donald_Trump_official_portrait.jpg/250px-Donald_Trump_official_portrait.jpg",
+      upvotes: "2.4K",
+      comments: "1.1K",
+      imageUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Donald_Trump_official_portrait.jpg/250px-Donald_Trump_official_portrait.jpg",
+      description: "",
+    },
+    {
+      subChubuForum: "r/AskChubuForum",
+      timeAgo: "8 hr. ago",
+      title:
+        "You're the last men/women on earth. Everyone else has the opposite sex. What do you do?",
+      upvotes: "3.6K",
+      comments: "5.2K",
+      imageUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Donald_Trump_official_portrait.jpg/250px-Donald_Trump_official_portrait.jpg",
+      description: "",
+    },
+    {
+      subChubuForum: "r/baseball",
+      timeAgo: "2 hr. ago",
+      title:
+        "[Highlight] Back to back to back home run from Shohei Ohtani, Mookie Betts & Freddie Freeman",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Donald_Trump_official_portrait.jpg/250px-Donald_Trump_official_portrait.jpg",
+      upvotes: "2.4K",
+      comments: "1.1K",
+      imageUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Donald_Trump_official_portrait.jpg/250px-Donald_Trump_official_portrait.jpg",
+      description: "",
+    },
   ];
 
   return (
