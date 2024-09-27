67b64da1d5bf51a94afe78bedde3a972db62fbb2
kijima-riku <riku.bulls.0116@icloud.com>
2024-09-28 05:48:29 +0900

    [add] mainLayout

--- forum-app/src/app/(main)/Layout.tsx
+++ forum-app/src/app/(main)/Layout.tsx
@@ -1,29 +1,18 @@
-"use client";
-
 import React, { ReactNode, useEffect } from "react";
 
 import Header from "@/components/Header/Header";
 import SideNavigation from "@/components/Header/SideNavigation";
 import PopularCommunities from "@/components/community/PopularCommunities";
-import { useRouter } from "next/navigation";
+import AuthCheck from "./AuthCheck";
 
 interface LayoutProps {
   children: ReactNode;
 }
 
-export default function Layout({ children }: LayoutProps): JSX.Element {
-  const router = useRouter();
-
-  // loginをせずに直接内部のURLを叩いた場合
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-    if (!token) {
-      router.push("/login");
-    }
-  }, [router]);
-
+export default function MainLayout({ children }: LayoutProps): JSX.Element {
   return (
     <div className="flex min-h-screen bg-gray-900 text-white">
+      <AuthCheck />
       <SideNavigation />
       <div className="flex-1">
         <Header />
